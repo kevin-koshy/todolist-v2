f87ee9f9b5f0d0a81046f3baf226c68294c67bf7
Kevin Koshy <kevin_koshy@hotmail.com>
2022-05-02 14:02:57 +0530

    Add update db functionality

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const workItems = [];
+
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
 const itemsSchema = {
@@ -31,7 +33,7 @@ const item2 = new Item({
 })
 
 const item3 = new Item({
-  name:"<-- Hit this to delete an itme>!"
+  name:"<-- Hit this to delete an item!"
 })
 
 const defaultItems = [item1, item2, item3];
@@ -58,15 +60,21 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item({
+      name:itemName
+  });
+  item.save();
+  res.redirect("/");
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
 });
 
 app.get("/work", function(req,res){
