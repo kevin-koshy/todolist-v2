2be8743564ae27e511c0349c99817a52ac7e789b
Kevin Koshy <kevin_koshy@hotmail.com>
2022-05-03 18:33:35 +0530

    Final Commit

--- app.js
+++ app.js
@@ -82,11 +82,11 @@ app.post("/", function(req, res){
         res.redirect("/");
   }
   else{
-      List.findOne(({name:listName}, function(err, foundList){
+      List.findOne({name:listName}, function(err, foundList){
           foundList.items.push(item);
           foundList.save();
           res.redirect("/"+listName);
-      }))
+      })
   }
 });
 
@@ -112,31 +112,32 @@ app.post("/delete", function (req, res){
              }
          });
      }
-
-
 })
 
 //route parameters
-app.get("/:customListName", function (req, res){
-    const customListName = _.capitalize(req.params.customListName)
+app.get("/:customListName", function(req, res){
+  const customListName = _.capitalize(req.params.customListName);
 
-    List.findOne({name:customListName}, function (err, results){
-        if(!err){
-            if(!results){
-                const list = new List({
-                    name:customListName,
-                    items:defaultItems
-                });
-                list.save();
-                res.redirect("/"+customListName);
-            }
-            else{
-                    res.render("list", {listTitle:results.name, newListItems:results.items});
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        //Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        //Show an existing list
 
-            }
-        }
-    })
-})
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+
+
+});
 
 app.get("/about", function(req, res){
   res.render("about");
